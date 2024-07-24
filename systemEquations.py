13de8fe278b15111c51df59c46caa2f3fd1a8266
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-07-24 13:08:07 +0200

    start3

--- systemEquations.py
+++ systemEquations.py
@@ -1,3 +1,20 @@
 import z3
+from z3 import Int, Solver
+
+
+x = Int('x')
+y = Int('y')
+s = Solver()
+s.add(x > 6)
+s.add(y > 9)
+s.add(x + y == 0)
+
+if s.check() == z3.sat:
+    model = s.model()
+    print(f"x = {model[x]}")
+    print(f"y = {model[y]}")
+else:
+    print("nema resenja")
+
 
 
